6a14faac237a51249b0a23edc0b0e7e6466a4970
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-14 13:06:36 +0500

    add reseller language file for penalties and wallet management

---
+++ platform/plugins/ecommerce/resources/lang/en/reseller.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'name' => 'Resellers',
+    'penalties' => [
+        'name' => 'Reseller Penalties',
+        'create' => 'Issue Penalty',
+        'view' => 'View Penalty #:id',
+        'reseller' => 'Reseller',
+        'order' => 'Order',
+        'product' => 'Product',
+        'amount' => 'Amount',
+        'reason' => 'Reason',
+        'issued_by' => 'Issued By',
+        'reverse' => 'Reverse Penalty',
+        'reversed_success' => 'Penalty reversed successfully',
+        'already_reversed' => 'This penalty has already been reversed',
+    ],
+    'wallet' => [
+        'balance' => 'Wallet Balance',
+        'negative_balance' => 'Negative Balance',
+        'blocked' => 'Your wallet is blocked due to negative balance',
+        'repay_required' => 'Please repay your balance to continue',
+    ],
+];
