58b0fd2a178b1a2bba4a2ebb86aa9a899ec7812a
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-14 13:41:20 +0500

    add product-countries language file for managing product availability by country

---
+++ platform/plugins/ecommerce/resources/lang/en/product-countries.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'name' => 'Product Countries',
+    'manage' => 'Manage Countries for :name',
+    'available_countries' => 'Available Countries',
+    'help_text' => 'Select the countries where this product can be sold. Products will only be visible to customers in the selected countries.',
+    'updated_successfully' => 'Product countries updated successfully.',
+    'bulk_updated_successfully' => 'Successfully updated countries for :count product(s).',
+    'country_not_available' => 'The selected country is not available.',
+    'country_updated' => 'Your country has been updated to :country. Products will now filter based on your selection.',
+    'no_countries_available' => 'No countries are available. Please add countries in the Location settings first.',
+    'not_available_in_country' => 'This product is not available in :country.',
+    'not_available_in_your_region' => 'This product is not available in your region.',
+    'available_in_country' => 'Available in :country',
+    'deliver_to' => 'Deliver to',
+    'select_your_country' => 'Select Your Country',
+    'choose_country' => 'Choose your country to see available products',
+    'change_country' => 'Change Country',
+    'current_country' => 'Current Country',
+];
