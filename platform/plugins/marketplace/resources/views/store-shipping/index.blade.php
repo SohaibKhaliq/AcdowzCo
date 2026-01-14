1c3db81c4c58b2d2e647919b89f227f0f0e99463
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-14 13:44:40 +0500

    add view for managing store shipping countries with country selection

---
+++ platform/plugins/marketplace/resources/views/store-shipping/index.blade.php
@@ -0,0 +1,59 @@
+@extends(BaseHelper::getAdminMasterLayoutTemplate())
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">
+                        <i class="ti ti-truck"></i>
+                        {{ trans('plugins/marketplace::store-shipping.manage', ['name' => $store->name]) }}
+                    </h4>
+                </div>
+                
+                <form action="{{ route('marketplace.store.shipping-countries.store', $store->id) }}" method="POST">
+                    @csrf
+                    
+                    <div class="card-body">
+                        <div class="alert alert-info">
+                            <i class="ti ti-info-circle"></i>
+                            {{ trans('plugins/marketplace::store-shipping.help_text') }}
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label">{{ trans('plugins/marketplace::store-shipping.shipping_countries') }}</label>
+                            <div class="row">
+                                @foreach($availableCountries as $country)
+                                    <div class="col-md-4 col-sm-6 mb-2">
+                                        <div class="form-check">
+                                            <input 
+                                                class="form-check-input" 
+                                                type="checkbox" 
+                                                name="countries[]" 
+                                                value="{{ $country->id }}"
+                                                id="country_{{ $country->id }}"
+                                                @checked(in_array($country->id, $assignedCountries))
+                                            >
+                                            <label class="form-check-label" for="country_{{ $country->id }}">
+                                                {{ $country->name }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer text-end">
+                        <a href="{{ route('marketplace.store.index') }}" class="btn btn-secondary">
+                            {{ trans('core/base::forms.cancel') }}
+                        </a>
+                        <button type="submit" class="btn btn-primary">
+                            {{ trans('core/base::forms.save') }}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
