1e8824bad909062d914376f3c6c13167b55aefe6
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-14 15:06:13 +0500

    add: update dashboard menu registration for reseller commissions and penalties with route checks

--- platform/plugins/ecommerce/src/Providers/EcommerceServiceProvider.php
+++ platform/plugins/ecommerce/src/Providers/EcommerceServiceProvider.php
@@ -939,24 +939,28 @@ class EcommerceServiceProvider extends ServiceProvider
                     'url' => fn() => route('customers.index'),
                     'permissions' => ['customers.index'],
                 ])
-                ->registerItem([
-                    'id' => 'cms-plugins-ecommerce-reseller-commissions',
-                    'priority' => 200,
-                    'parent_id' => 'cms-plugins-ecommerce',
-                    'name' => 'plugins/ecommerce::ecommerce.reseller-commissions',
-                    'icon' => 'ti ti-percentage',
-                    'url' => fn() => route('reseller-commissions.index'),
-                    'permissions' => ['customers.index'],
-                ])
-                ->registerItem([
-                    'id' => 'cms-plugins-ecommerce-reseller-penalties',
-                    'priority' => 201,
-                    'parent_id' => 'cms-plugins-ecommerce',
-                    'name' => 'plugins/ecommerce::reseller.penalties.name',
-                    'icon' => 'ti ti-alert-circle',
-                    'url' => fn() => route('ecommerce.reseller-penalties.index'),
-                    'permissions' => ['customers.index'],
-                ])
+                ->when(Route::has('reseller-commissions.index') || Route::has('ecommerce.reseller-commissions.index'), function (DashboardMenuSupport $dashboardMenu): void {
+                    $dashboardMenu->registerItem([
+                        'id' => 'cms-plugins-ecommerce-reseller-commissions',
+                        'priority' => 200,
+                        'parent_id' => 'cms-plugins-ecommerce',
+                        'name' => 'plugins/ecommerce::ecommerce.reseller-commissions',
+                        'icon' => 'ti ti-percentage',
+                        'url' => fn() => route(Route::has('reseller-commissions.index') ? 'reseller-commissions.index' : 'ecommerce.reseller-commissions.index'),
+                        'permissions' => ['customers.index'],
+                    ]);
+                })
+                ->when(Route::has('reseller-penalties.index') || Route::has('ecommerce.reseller-penalties.index'), function (DashboardMenuSupport $dashboardMenu): void {
+                    $dashboardMenu->registerItem([
+                        'id' => 'cms-plugins-ecommerce-reseller-penalties',
+                        'priority' => 201,
+                        'parent_id' => 'cms-plugins-ecommerce',
+                        'name' => 'plugins/ecommerce::reseller.penalties.name',
+                        'icon' => 'ti ti-alert-circle',
+                        'url' => fn() => route(Route::has('reseller-penalties.index') ? 'reseller-penalties.index' : 'ecommerce.reseller-penalties.index'),
+                        'permissions' => ['customers.index'],
+                    ]);
+                })
                 ->when(EcommerceHelper::isProductSpecificationEnabled(), function (DashboardMenuSupport $dashboardMenu): void {
                     $dashboardMenu
                         ->registerItem([
