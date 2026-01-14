2d529cc1ca15f57eb55a491eafa9a99eb956035f
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-14 13:44:33 +0500

    add language file for managing store shipping countries

---
+++ platform/plugins/marketplace/resources/lang/en/store-shipping.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name' => 'Store Shipping',
+    'manage' => 'Manage Shipping Countries for :name',
+    'shipping_countries' => 'Shipping Countries',
+    'help_text' => 'Select the countries where this store can ship products. Store products will only be visible to customers in the selected countries.',
+    'updated_successfully' => 'Shipping countries updated successfully.',
+    'bulk_updated_successfully' => 'Successfully updated shipping countries for :count store(s).',
+    'vendor_title' => 'My Shipping Countries',
+    'vendor_help_text' => 'Select the countries where you can ship your products. Your products will only be visible to customers in these countries.',
+    'vendor_updated_successfully' => 'Your shipping countries have been updated successfully.',
+];
