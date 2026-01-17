4dbb0036fcb43b0fabe511c9c0e63cefa1a67022
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2026-01-17 23:00:58 +0500

    feat: Implement enhanced customer login with OTP, reseller application management, vendor warnings, and API features.

---
+++ platform/plugins/ecommerce/resources/views/emails/reseller-account-disabled.blade.php
@@ -0,0 +1,11 @@
+{{ header }}
+
+<h2>Reseller Account Disabled</h2>
+
+<p>Dear {{ $customer_name }},</p>
+
+<p>Your reseller account (ID: {{ $reseller_id }}) has been disabled.</p>
+
+<p>If you have any questions, please contact our support team.</p>
+
+{{ footer }}
